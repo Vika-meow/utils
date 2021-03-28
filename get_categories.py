ca1c9c32d3b471115d7da01440ab411af64f1f66
Vika-meow <viktoriagnezdilova@gmail.com>
2021-03-28 20:58:29 +0700

    new algo

--- get_categories.py
+++ get_categories.py
@@ -1,5 +1,14 @@
 import argparse
 
+
+def print_dict_with_set(dic=dict()):
+    for x, y in dic.items():
+        el = ""
+        for z in y:
+            el += z + " "
+        print(str(x) + " " + el)
+
+
 parser = argparse.ArgumentParser(description='Get pair entity-category')
 parser.add_argument('-f', '--file', help='Entities file', default="kg1_ent_ids")
 parser.add_argument('-c', '--categories', help='Categories file', default="article_categories_en.ttl")
@@ -19,7 +28,6 @@ with open(args.categories, encoding='utf-8') as cat_file:
 print("categories loaded")
 
 ent_cat_less = dict()
-counter = 1
 print("start load entities")
 with open(args.file, encoding='utf-8') as f:
     for line in f:
@@ -28,13 +36,47 @@ with open(args.file, encoding='utf-8') as f:
             ent_cat_less[ent] = ent_cat[ent]
         else:
             ent_cat_less[ent] = set()
-
+print_dict_with_set(ent_cat)
+print_dict_with_set(ent_cat_less)
 ent_cat.clear()
 print("entities loaded")
 categories = dict()
 counter = 0
 flag = 0
 
+print("start collect categories")
+for x in ent_cat_less.values():
+    for y in categories.values():
+        for cat in x:
+            if cat in y:
+                flag = 1
+                y.update(x)
+                break
+        if flag == 1:
+            break
+    if flag == 0:
+        counter += 1
+        categories[counter] = x
+    flag = 0
+    print(str(counter))
+
+print("end collect categories")
+#print_dict_with_set(categories)
+print("start writing file")
+with open(args.output, 'w+') as out:
+    for key, values in ent_cat_less.items():
+        if len(values) != 0:
+            for num, cats in categories.items():
+                for val in values:
+                    if val in cats:
+                        out.write(str(num) + '\n')
+                        #out.write(key + '\t' + str(num) + '\n')
+                        break
+        else:
+            out.write(str(0) + '\n')
+            #out.write(key + '\t' + str(0) + '\n')
+print("end writing file")
+'''
 print("start collect categories for each entity")
 # для каждого элемента собираем категории, которые используются вместе
 for x in ent_cat_less.values():
@@ -42,6 +84,8 @@ for x in ent_cat_less.values():
         for cat in y:
             if cat in x:
                 x.update(y)
+                break
+    #print("entity " + x + " done")
 
 i = 0
 for x, y in ent_cat_less.items():
@@ -84,6 +128,7 @@ with open(args.output, 'w+') as out:
             #out.write(key + '\t' + str(0) + '\n')
 
 print("end writing file")
+'''
 #with open(args.meta, 'w+') as out:
 #    for x, y in categories.items():
 #        el = ""
